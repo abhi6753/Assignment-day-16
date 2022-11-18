ec3304fd1bbc4b72c84d8295889e98f344894af0
Abhishek kumar <113863385+abhi6753@users.noreply.github.com>
2022-11-18 23:34:27 +0530

    Update question_6.c

--- question_6.c
+++ question_6.c
@@ -3,17 +3,47 @@
 #include<conio.h>
 int main()
 {
-    int row,col;
+    int row,col,i,j,sum;
     printf("Enter number of row and column:\n");
     scanf("%d %d",&row,&col);
-    int arr[row][col];
+    int matrix[row][col];
     printf("Enter %d elements:\n",row*col);
     for(i=0;i<row;i++)
     {
         for(j=0;j<col;j++)
         {
-            scanf("%d",&arr[i][j]);
+            scanf("%d",&matrix[i][j]);
         }
     }
-    printf("Sum of total row = %d",sum);
+    printf("\nMATRX :\n");
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            printf("%d ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("SUM OF ROW:\n");
+    for(i=0;i<row;i++)
+    {
+        sum =0;
+        for(j=0;j<col;j++)
+        {
+            sum+=matrix[i][j];
+        }
+        printf("\tSum of row %d = %d\n",i+1,sum);
+    }
+    printf("SUM OF COLUMN:\n");
+    for(i=0;i<row;i++)
+    {
+        sum =0;
+        for(j=0;j<col;j++)
+        {
+            sum+=matrix[j][i];
+        }
+        printf("\tSum of column %d = %d\n",i+1,sum);
+    }
+    getch();
+    return 0;
 }
